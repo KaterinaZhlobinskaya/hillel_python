29da8fc29e55664f770d6c7141a3172989b97678
Ekaterina Zhlobinskaya <evil.666.buny@gmail.com>
2022-08-08 23:14:56 +0300

    update file 1

--- lesson_5/1.py
+++ lesson_5/1.py
@@ -1,1 +1,14 @@
-print("hello world 1")
+name = "John Doe"
+for char in name:
+    print(char)
+
+data = ["John", "Doe"]
+for char in data:
+    print(char)
+
+#flag = True
+#index = 0
+#while flag: 
+#    element = data[index]
+#   print(element)
+#    index += 1
