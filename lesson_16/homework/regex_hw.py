fc758afd4ea72ef0ee6d378fd00ff16590a3f7d5
Ekaterina Zhlobinskaya <evil.666.buny@gmail.com>
2022-09-06 21:32:56 +0300

    upload lesson 16

---
+++ lesson_16/homework/regex_hw.py
@@ -0,0 +1,21 @@
+import re
+
+"""
+Завершіть функцію/метод, щоб він повернув URL-адресу з будь-яким після видалення зв’язку (#).
+Що то таке HTML Anchor - https://www.semrush.com/blog/html-anchor/
+
+Examples
+"lms.ithillel.ua#about" --> "lms.ithillel.ua"
+"lms.ithillel.ua?page=1" -->lms.ithillel.ua?page=1"
+
+"""
+
+
+def remove_url_anchor(url):
+    return re.split("#", url)[0]
+    pass
+
+
+assert remove_url_anchor("lms.ithillel.ua#about") == "lms.ithillel.ua"
+assert remove_url_anchor("lms.ithillel.ua/groups/?page=1#example") == "lms.ithillel.ua/groups/?page=1"
+assert remove_url_anchor("lms.ithillel.ua/groups/") == "lms.ithillel.ua/groups/"
